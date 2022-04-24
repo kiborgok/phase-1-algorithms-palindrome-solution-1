071767cc0cc12577081c147088a1fe5c91db14de
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-24 12:41:51 +0300

    add solution 1

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word).toLowerCase();
+  return word.toLowerCase() === reversedWord;
+}
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
+  string input
+  if string input is equal to reversed string input
+    return true
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  isPalindrome() should take a str input and return true if it can be read the same even if reversed 
+  and return false if it can not be read the same when reversed
 */
 
 // You can run `node index.js` to view these console logs
@@ -19,7 +31,27 @@ if (require.main === module) {
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("alex"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("refer"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("civic"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("key"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("wq"));
 }
 
 module.exports = isPalindrome;
